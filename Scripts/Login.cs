2e58e947dd964d9ff705d7927b41547a79488642
Nick0108 <zidonglin@outlook.com>
2017-06-27 10:31:56 +0800

    Feat: Add a Timer to count down

---
+++ Scripts/Login.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour {
+    void EntertheCarnival()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("Carnival");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        EntertheCarnival();
+
+    }
+}
---
+++ Scripts/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public Text RemainingTimer;
+    public Slider TimeSlider;
+    public int RemainingTimerNum = 600;
+    public CarnivalScores carnivalScores;
+    public Text Score;
+    public GameObject GameOverPanel;
+
+    private float timer;
+    private int totalScore;
+
+    void BackToLogin()
+    {
+        SceneManager.LoadScene("Login");
+    }
+
+	void Start () {
+        RemainingTimer.text = RemainingTimerNum.ToString();
+        timer = 0.0f;
+        TimeSlider.maxValue = RemainingTimerNum;
+        TimeSlider.minValue = 0.0f;
+        totalScore = 0;
+    }
+	
+	
+	void Update () {
+        if (timer > 1.0f)
+        {
+            RemainingTimerNum--;
+            if (RemainingTimerNum <= 0)
+                RemainingTimerNum = 0;
+            RemainingTimer.text = RemainingTimerNum.ToString();
+            TimeSlider.value = (float)RemainingTimerNum;
+            timer = 0.0f;
+        }
+        timer += Time.deltaTime;
+        if (RemainingTimerNum <= 0)
+        {
+            totalScore = carnivalScores.TotalScore();
+            Score.text = totalScore.ToString();
+            GameOverPanel.SetActive(true);
+            Invoke("BackToLogin", 5.0f);
+        }
+	}
+}
